9547a8abc0bf0c45bf0c8be56f9580798835acba
Gabriel Fratelli <gfc@lionx.com.br>
2022-04-29 13:14:43 -0300

    Reajust Project
    falta resolver o funcionamento do envio de mensagem.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { AuthProvider } from "./contexts/auth";
 
-import './styles/global.css'
+import "./styles/global.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
-)
+);
